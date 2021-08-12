686af24f2810b3c64803e5b52bb1aafacef15bf1
christoforosl <christoforosl08@gmail.com>
2021-08-12 11:12:11 +0300

    Revert "new images and removed checkbox"
    This reverts commit 3ccc65a7890be74060a8c4207800e15f7d852e31.

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,33 @@
 // note: JQuery is not available to this backround js script
 var curentDateTimeTimer = null;
 var settings;
+const randomImages = [
+  {title:"Three Men Standing Near Waterfalls", url:"https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&dpr=3"},
+  {title:"Body of Water Near Brown Sand", url:"https://images.pexels.com/photos/7999461/pexels-photo-7999461.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Brown Mountain", url:"https://images.pexels.com/photos/3308741/pexels-photo-3308741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Porto, Portugal", url:"https://images.pexels.com/photos/2549156/pexels-photo-2549156.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Yosemite Valley, United States", url:"https://images.pexels.com/photos/1571108/pexels-photo-1571108.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Torres del Paine, Chile", url:"https://images.pexels.com/photos/3739624/pexels-photo-3739624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Florence, Italy", url:"https://images.pexels.com/photos/4015473/pexels-photo-4015473.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Hlavní město Praha, Czechia", url:"https://images.pexels.com/photos/783739/pexels-photo-783739.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"New York", url:"https://images.pexels.com/photos/3875821/pexels-photo-3875821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Stop Wishing...", url:"https://images.pexels.com/photos/2045600/pexels-photo-2045600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Huangpu Qu, China", url:"https://images.pexels.com/photos/842654/pexels-photo-842654.jpeg?cs=srgb&dl=pexels-zhang-kaiyv-842654.jpg&fm=jpg"},
+  {title:"Guatemala", url:"https://images.pexels.com/photos/2661176/pexels-photo-2661176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {title:"Batang Kali, Malaysia", url:"https://images.pexels.com/photos/1173777/pexels-photo-1173777.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"},
+  {title:"Maldives", url:"https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {"title":"Cyprus coast","url":"https://images.pexels.com/photos/6860099/pexels-photo-6860099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {"title":"Morskie Oko, Sea Eye, Tatra National Park, Poland","url":"https://st3.depositphotos.com/14847044/i/600/depositphotos_178404276-stock-photo-view-rocky-shore-stones-water.jpg"},
+  {"title":"Serpentine","url":"https://st4.depositphotos.com/18241762/i/600/depositphotos_202584372-stock-photo-serpentine.jpg"},
+  {"title":"Green Fields Near Brown Mountain","url":"https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  {"photographer":"Brett Sayles","title":"White Mountains Under White and Gray Sky","url":"https://images.pexels.com/photos/1701188/pexels-photo-1701188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  { "photographer":"Olga Lioncat",
+    "title":"Cathedral and Ferris wheel in London",
+    "url":"https://images.pexels.com/photos/7245352/pexels-photo-7245352.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+    { "photographer":"Rachel Claire",
+    "title":"Rome",
+    "url":"https://images.pexels.com/photos/4819654/pexels-photo-4819654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+];
 
 const CALL_QUOTE_HEADERS = new Headers({
   "accept": "application/json",
@@ -36,7 +63,7 @@ function setQuote() {
   chrome.storage.local.get(["quote","quoteDate","quoteBy"],function(value) {
     
     var oldq = getQuoteIsOld(value ? value.quoteDate: null);
-    //console.log('oldq xxx' +  oldq);
+    
     if (!value || value.length ===0  || value.quote==='' || oldq) {
       setQuoteFromService();
     } else {
@@ -95,7 +122,7 @@ function setCurrentDateTime() {
 
 function initBackground() {
 
-  //console.log("init backround");
+  
   if(!settings) {
     console.log("loading settings");
     settings = JSON.parse( window.localStorage.getItem("settings") ) || {"imageKeywords":"nature"};
@@ -145,20 +172,45 @@ function fetchImageFromApiService() {
   fetch(myRequest)
     .then(response => response.json())
     .then(contents => {
-      var currentBackroundImage = {};
+      
       var photo = contents;
+      var currentBackroundImage = {};
       currentBackroundImage.photographer = photo.user.name;
       currentBackroundImage.photographerUrl = photo.user.portfolio_url;
       currentBackroundImage.src = photo.urls.full;
-      //currentBackroundImage.nextPhotoPage = contents.next_page;
       currentBackroundImage.setDate = new Date().toDateString();
       currentBackroundImage.location = photo.location ? photo.location.title : '';
       currentBackroundImage.description = photo.alt_description ? photo.alt_description : photo.description;
-      
+     
       localStorage.setItem('currentBackroundImage', JSON.stringify(currentBackroundImage));
       setBackroundImageFromStorage(currentBackroundImage);
+
+    })
+    .catch(() =>{
+      const currentImageIndexParsed =  isPositiveInteger(localStorage.getItem('currentBackroundImageIndex')) 
+            ? parseInt(localStorage.getItem('currentBackroundImageIndex')) + 1 
+            : 0;
+      
+      const currentImageIndex = currentImageIndexParsed > randomImages.length-1 ? 0 :currentImageIndexParsed ;
+
+      var currentBackroundImage = {};
+      currentBackroundImage.photographer = "Unknown";
+      currentBackroundImage.src = randomImages[currentImageIndex].url;
+      currentBackroundImage.photographer = randomImages[currentImageIndex].photographer;
+      currentBackroundImage.setDate = new Date().toDateString();
+      currentBackroundImage.location =  `${randomImages[currentImageIndex].title}, Image index: ${currentImageIndex}`;
+      currentBackroundImage.description = '';
+      
+      localStorage.setItem('currentBackroundImage', JSON.stringify(currentBackroundImage));
+      localStorage.setItem('currentBackroundImageIndex', currentImageIndex.toString());
+      setBackroundImageFromStorage(currentBackroundImage);
+
     });
-  
+    
+}
+
+function isPositiveInteger(n) {
+  return n >>> 0 === parseFloat(n);
 }
 
 function arrayOfItems() {
@@ -192,15 +244,6 @@ chrome.runtime.onInstalled.addListener(function () {
 
 initBackground();
 
-if($e("btnEndTimer")) {
-  $e("btnEndTimer").addEventListener("click", endFocusTimer);
-}
-if($e("btnPauseFocusTimer")) {
-  $e("btnPauseFocusTimer").addEventListener("click", togglePauseStatus);
-}
-
-
-
 if($e("btnSetCurrentFocusAndStartTimer")) {
   $e("btnSetCurrentFocusAndStartTimer").addEventListener("click", SetCurrentFocusAndStartTimer);
 
@@ -222,7 +265,7 @@ if($e("btnShowSettings")) {
 
 if($e("btnChangeWallpaper")) {
   $e("btnChangeWallpaper").addEventListener("click", function(){
-    var currentBackroundImage = JSON.parse(localStorage.getItem('currentBackroundImage')) || {};
+    
     localStorage.removeItem('currentBackroundImage');
     fetchImageFromApiService();
 
