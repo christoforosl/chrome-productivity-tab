5c9a61101980d897de311da555f36d95f77d79bf
christoforosl <christoforosl08@gmail.com>
2021-08-13 09:21:06 +0300

    last inactivity time

--- scripts/background.js
+++ scripts/background.js
@@ -1,33 +1,37 @@
-/* jshint esversion: 6 */ 
+/* jshint esversion: 6 */
 // note: JQuery is not available to this backround js script
 var curentDateTimeTimer = null;
 var settings;
 const randomImages = [
-  {title:"Three Men Standing Near Waterfalls", url:"https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&dpr=3"},
-  {title:"Body of Water Near Brown Sand", url:"https://images.pexels.com/photos/7999461/pexels-photo-7999461.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Brown Mountain", url:"https://images.pexels.com/photos/3308741/pexels-photo-3308741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Porto, Portugal", url:"https://images.pexels.com/photos/2549156/pexels-photo-2549156.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Yosemite Valley, United States", url:"https://images.pexels.com/photos/1571108/pexels-photo-1571108.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Torres del Paine, Chile", url:"https://images.pexels.com/photos/3739624/pexels-photo-3739624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Florence, Italy", url:"https://images.pexels.com/photos/4015473/pexels-photo-4015473.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Hlavní město Praha, Czechia", url:"https://images.pexels.com/photos/783739/pexels-photo-783739.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"New York", url:"https://images.pexels.com/photos/3875821/pexels-photo-3875821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Stop Wishing...", url:"https://images.pexels.com/photos/2045600/pexels-photo-2045600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Huangpu Qu, China", url:"https://images.pexels.com/photos/842654/pexels-photo-842654.jpeg?cs=srgb&dl=pexels-zhang-kaiyv-842654.jpg&fm=jpg"},
-  {title:"Guatemala", url:"https://images.pexels.com/photos/2661176/pexels-photo-2661176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {title:"Batang Kali, Malaysia", url:"https://images.pexels.com/photos/1173777/pexels-photo-1173777.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"},
-  {title:"Maldives", url:"https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {"title":"Cyprus coast","url":"https://images.pexels.com/photos/6860099/pexels-photo-6860099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {"title":"Morskie Oko, Sea Eye, Tatra National Park, Poland","url":"https://st3.depositphotos.com/14847044/i/600/depositphotos_178404276-stock-photo-view-rocky-shore-stones-water.jpg"},
-  {"title":"Serpentine","url":"https://st4.depositphotos.com/18241762/i/600/depositphotos_202584372-stock-photo-serpentine.jpg"},
-  {"title":"Green Fields Near Brown Mountain","url":"https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  {"photographer":"Brett Sayles","title":"White Mountains Under White and Gray Sky","url":"https://images.pexels.com/photos/1701188/pexels-photo-1701188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-  { "photographer":"Olga Lioncat",
-    "title":"Cathedral and Ferris wheel in London",
-    "url":"https://images.pexels.com/photos/7245352/pexels-photo-7245352.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    { "photographer":"Rachel Claire",
-    "title":"Rome",
-    "url":"https://images.pexels.com/photos/4819654/pexels-photo-4819654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+  { title: "Three Men Standing Near Waterfalls", url: "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&dpr=3" },
+  { title: "Body of Water Near Brown Sand", url: "https://images.pexels.com/photos/7999461/pexels-photo-7999461.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Brown Mountain", url: "https://images.pexels.com/photos/3308741/pexels-photo-3308741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Porto, Portugal", url: "https://images.pexels.com/photos/2549156/pexels-photo-2549156.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Yosemite Valley, United States", url: "https://images.pexels.com/photos/1571108/pexels-photo-1571108.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Torres del Paine, Chile", url: "https://images.pexels.com/photos/3739624/pexels-photo-3739624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Florence, Italy", url: "https://images.pexels.com/photos/4015473/pexels-photo-4015473.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Hlavní město Praha, Czechia", url: "https://images.pexels.com/photos/783739/pexels-photo-783739.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "New York", url: "https://images.pexels.com/photos/3875821/pexels-photo-3875821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Stop Wishing...", url: "https://images.pexels.com/photos/2045600/pexels-photo-2045600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Huangpu Qu, China", url: "https://images.pexels.com/photos/842654/pexels-photo-842654.jpeg?cs=srgb&dl=pexels-zhang-kaiyv-842654.jpg&fm=jpg" },
+  { title: "Guatemala", url: "https://images.pexels.com/photos/2661176/pexels-photo-2661176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { title: "Batang Kali, Malaysia", url: "https://images.pexels.com/photos/1173777/pexels-photo-1173777.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" },
+  { title: "Maldives", url: "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { "title": "Cyprus coast", "url": "https://images.pexels.com/photos/6860099/pexels-photo-6860099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { "title": "Morskie Oko, Sea Eye, Tatra National Park, Poland", "url": "https://st3.depositphotos.com/14847044/i/600/depositphotos_178404276-stock-photo-view-rocky-shore-stones-water.jpg" },
+  { "title": "Serpentine", "url": "https://st4.depositphotos.com/18241762/i/600/depositphotos_202584372-stock-photo-serpentine.jpg" },
+  { "title": "Green Fields Near Brown Mountain", "url": "https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  { "photographer": "Brett Sayles", "title": "White Mountains Under White and Gray Sky", "url": "https://images.pexels.com/photos/1701188/pexels-photo-1701188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+  {
+    "photographer": "Olga Lioncat",
+    "title": "Cathedral and Ferris wheel in London",
+    "url": "https://images.pexels.com/photos/7245352/pexels-photo-7245352.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+  },
+  {
+    "photographer": "Rachel Claire",
+    "title": "Rome",
+    "url": "https://images.pexels.com/photos/4819654/pexels-photo-4819654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+  },
 ];
 
 const CALL_QUOTE_HEADERS = new Headers({
@@ -52,32 +56,32 @@ function setCurrentDateTimeTimer() {
  * @param {long} quoteDate: the quote's set time, in epoch seconds
  */
 function getQuoteIsOld(quoteDate) {
-  if(!quoteDate)return true;
+  if (!quoteDate) return true;
   var dt = new Date().toDateString();
   return dt !== quoteDate;
 
-} 
+}
 
 function setQuote() {
 
-  chrome.storage.local.get(["quote","quoteDate","quoteBy"],function(value) {
-    
-    var oldq = getQuoteIsOld(value ? value.quoteDate: null);
-    
-    if (!value || value.length ===0  || value.quote==='' || oldq) {
+  chrome.storage.local.get(["quote", "quoteDate", "quoteBy"], function (value) {
+
+    var oldq = getQuoteIsOld(value ? value.quoteDate : null);
+
+    if (!value || value.length === 0 || value.quote === '' || oldq) {
       setQuoteFromService();
     } else {
-      console.log('Set quote from storage :-)' );
+      console.log('Set quote from storage :-)');
       $html("quote", '\"' + value.quote + '\"');
       $html("quoteBy", value.quoteBy);
-    } 
+    }
   });
-          
+
 }
 
 
 function setQuoteFromService() {
-  
+
   var myRequest = new Request(options.APIQuoteOfTheDayApiHost, {
     "method": "GET",
     "headers": CALL_QUOTE_HEADERS,
@@ -103,55 +107,55 @@ function setQuoteFromService() {
 
 function setCurrentDateTime() {
   var d = new Date();
-  var t = d.toDateString() + ', ' + d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  var t = d.toDateString() + ', ' + d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   var greeting;
 
   if (d.getHours() > 0 && d.getHours() <= 12) {
     greeting = "Good Morning, ";
   } else if (d.getHours() > 12 && d.getHours() <= 19) {
-    greeting = "Good Afternoon, " ;
+    greeting = "Good Afternoon, ";
   } else {
-    greeting = "Good Evening, " ;
+    greeting = "Good Evening, ";
   }
   greeting = greeting + (settings.greetingName || "[Specify Name In Settings]");
-  $html('btnSetWorkItem',  options.whatShallWeWorkOnQuestionText);
-  $html("currentTime", t );
-  $html("greeting", greeting );
-  
+  $html('btnSetWorkItem', options.whatShallWeWorkOnQuestionText);
+  $html("currentTime", t);
+  $html("greeting", greeting);
+
 }
 
 function initBackground() {
 
-  
-  if(!settings) {
+
+  if (!settings) {
     console.log("loading settings");
-    settings = JSON.parse( window.localStorage.getItem("settings") ) || {"imageKeywords":"nature"};
-    if(!settings){
+    settings = JSON.parse(window.localStorage.getItem("settings")) || { "imageKeywords": "nature" };
+    if (!settings) {
       settings = {};
     }
   }
   checkBackroundImageOnLoad();
-  noFocusTimerUI() ;
+  noFocusTimerUI();
   checkForActiveFocusTimer();
   setCurrentDateTimeTimer();
   setQuote();
-  
+
   chrome.tabs.onRemoved.addListener(function () {
     if (curentDateTimeTimer) {
       console.log("Clearing Interval");
       clearInterval(curentDateTimeTimer);
     }
   });
-  
+
 }
 
 function checkBackroundImageOnLoad() {
 
   if (window.jQuery) {
-    $(document).ready(function(){
+    $(document).ready(function () {
       var currentBackroundImage = JSON.parse(localStorage.getItem('currentBackroundImage')) || {};
       var setImageFromStorage = currentBackroundImage.src && (currentBackroundImage.setDate === new Date().toDateString());
-      if ( setImageFromStorage ) {
+      if (setImageFromStorage) {
         setBackroundImageFromStorage(currentBackroundImage);
       } else {
         fetchImageFromApiService();
@@ -172,7 +176,7 @@ function fetchImageFromApiService() {
   fetch(myRequest)
     .then(response => response.json())
     .then(contents => {
-      
+
       var photo = contents;
       var currentBackroundImage = {};
       currentBackroundImage.photographer = photo.user.name;
@@ -181,32 +185,32 @@ function fetchImageFromApiService() {
       currentBackroundImage.setDate = new Date().toDateString();
       currentBackroundImage.location = photo.location ? photo.location.title : '';
       currentBackroundImage.description = photo.alt_description ? photo.alt_description : photo.description;
-     
+
       localStorage.setItem('currentBackroundImage', JSON.stringify(currentBackroundImage));
       setBackroundImageFromStorage(currentBackroundImage);
 
     })
-    .catch(() =>{
-      const currentImageIndexParsed =  isPositiveInteger(localStorage.getItem('currentBackroundImageIndex')) 
-            ? parseInt(localStorage.getItem('currentBackroundImageIndex')) + 1 
-            : 0;
-      
-      const currentImageIndex = currentImageIndexParsed > randomImages.length-1 ? 0 :currentImageIndexParsed ;
+    .catch(() => {
+      const currentImageIndexParsed = isPositiveInteger(localStorage.getItem('currentBackroundImageIndex'))
+        ? parseInt(localStorage.getItem('currentBackroundImageIndex')) + 1
+        : 0;
+
+      const currentImageIndex = currentImageIndexParsed > randomImages.length - 1 ? 0 : currentImageIndexParsed;
 
       var currentBackroundImage = {};
       currentBackroundImage.photographer = "Unknown";
       currentBackroundImage.src = randomImages[currentImageIndex].url;
       currentBackroundImage.photographer = randomImages[currentImageIndex].photographer;
       currentBackroundImage.setDate = new Date().toDateString();
-      currentBackroundImage.location =  `${randomImages[currentImageIndex].title}, Image index: ${currentImageIndex}`;
+      currentBackroundImage.location = `${randomImages[currentImageIndex].title}, Image index: ${currentImageIndex}`;
       currentBackroundImage.description = '';
-      
+
       localStorage.setItem('currentBackroundImage', JSON.stringify(currentBackroundImage));
       localStorage.setItem('currentBackroundImageIndex', currentImageIndex.toString());
       setBackroundImageFromStorage(currentBackroundImage);
 
     });
-    
+
 }
 
 function isPositiveInteger(n) {
@@ -215,21 +219,21 @@ function isPositiveInteger(n) {
 
 function arrayOfItems() {
 
-	var finalArr = [];
+  var finalArr = [];
 
-	for (var i = 0; i < arguments.length; i++) {
-    if(arguments[i]) {
+  for (var i = 0; i < arguments.length; i++) {
+    if (arguments[i]) {
       finalArr.push(arguments[i]);
     }
-	}
-	return finalArr.join(',');
+  }
+  return finalArr.join(',');
 }
 
 function setBackroundImageFromStorage(currentBackroundImage) {
-  $("html").css("background-image", "url('"  + currentBackroundImage.src +"')");
+  $("html").css("background-image", "url('" + currentBackroundImage.src + "')");
   var photoInfo = 'Photo By ';
   if (currentBackroundImage.photographerUrl) {
-    photoInfo += '<a style="color:white" target="_new" href="'+currentBackroundImage.photographerUrl+'">'+currentBackroundImage.photographer+'</a>';
+    photoInfo += '<a style="color:white" target="_new" href="' + currentBackroundImage.photographerUrl + '">' + currentBackroundImage.photographer + '</a>';
   } else {
     photoInfo += currentBackroundImage.photographer;
   }
@@ -244,10 +248,10 @@ chrome.runtime.onInstalled.addListener(function () {
 
 initBackground();
 
-if($e("btnSetCurrentFocusAndStartTimer")) {
+if ($e("btnSetCurrentFocusAndStartTimer")) {
   $e("btnSetCurrentFocusAndStartTimer").addEventListener("click", SetCurrentFocusAndStartTimer);
 
-  $e("frmEnterTaskName").addEventListener('submit', function(event) {
+  $e("frmEnterTaskName").addEventListener('submit', function (event) {
     if ($e("frmEnterTaskName").checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
@@ -256,18 +260,54 @@ if($e("btnSetCurrentFocusAndStartTimer")) {
   }, false);
 
 }
-if($e("btnShowSettings")) {
+if ($e("btnShowSettings")) {
   $('#backroundImageSearchTerms').val(settings.imageKeywords);
   $('#greetingName').val(settings.greetingName);
-  
-  
+
+
 }
 
-if($e("btnChangeWallpaper")) {
-  $e("btnChangeWallpaper").addEventListener("click", function(){
-    
+if ($e("btnChangeWallpaper")) {
+  $e("btnChangeWallpaper").addEventListener("click", function () {
+
     localStorage.removeItem('currentBackroundImage');
     fetchImageFromApiService();
 
   });
 }
+
+
+// an array of epoch dates indicating the last 10 times the user was active.
+// this is a way to detect inactivity and register potential end time of focus task
+let lastActiveTime = 0;
+let previousActiveTime = 0;
+let potentialEndTimeDueToInactivity = 0;
+
+const inactivityTime = function () {
+  let time;
+  window.onload = resetTimer;
+  const events = ['mousedown', 'mousemove', 'keypress', 'scroll', 'touchstart'];
+
+  events.forEach(function (name) {
+    document.addEventListener(name, resetTimer, true);
+  });
+
+  function logout() {
+    previousActiveTime = lastActiveTime;
+    lastActiveTime = new Date().getTime();
+
+    if (lastActiveTime - previousActiveTime > 30 * 60 * 1000) {
+      potentialEndTimeDueToInactivity = previousActiveTime;
+      $('#potentialEndTimeDueToInactivity').html( new Date(potentialEndTimeDueToInactivity).toDateString() );
+    } else {
+      potentialEndTimeDueToInactivity = 0;
+    }
+  }
+
+  function resetTimer() {
+    clearTimeout(time);
+    time = setTimeout(logout, 10 * 60 * 1000); // set a ten minute timeout
+
+  }
+};
+
